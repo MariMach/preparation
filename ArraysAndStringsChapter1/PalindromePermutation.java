4b8be8a5d8213ceff4343465c526c26292a8cc43
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-02-05 13:56:12 +0100

    getPalindrome done

--- ArraysAndStringsChapter1/PalindromePermutation.java
+++ ArraysAndStringsChapter1/PalindromePermutation.java
@@ -30,10 +30,52 @@ public class PalindromePermutation {
             if (i % 2 != 0)
                 odds++;
         }
-        return odds == 1;
+        return odds <= 1;
+    }
+
+    public static String getPalindrome(String s) {
+        // case insensitive
+        // get rid of extra space
+        s = s.replaceAll(" +", "").toLowerCase();
+        int n = s.length();
+        int[] char_set = new int[256];
+        int odds = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i);
+            char_set[val]++;
+        }
+        for (int i : char_set) {
+            // 0 % 2 == 0
+            if (i % 2 != 0)
+                odds++;
+        }
+        if (odds <= 1) {
+            char[] result = new char[n];
+            int k = 0;
+            for (int i = 0; i < char_set.length; i++) {
+                if (char_set[i] != 0) {
+                    int midd = (int) n / 2;
+                    if (char_set[i] % 2 != 0) {
+                        result[midd] = (char) i;
+                    } else {
+                        while (k != midd && char_set[i] > 0) {
+                            result[k] = (char) i;
+                            result[n - 1 - k] = (char) i;
+                            char_set[i]--;
+                            char_set[i]--;
+                            k++;
+                        }
+                    }
+                }
+
+            }
+            return new String(result);
+        }
+        return "not a palindrome permutation";
     }
 
     public static void main(String[] args) {
         System.out.println(isPalindromePermutation("Tac t  Coa"));
+        System.out.println(getPalindrome("Tac t  Coa"));
     }
 }
