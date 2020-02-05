c677cec8b28efb33fdec29b9c12a650045636bd5
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-02-05 15:21:04 +0100

    zero matrix optimized code

--- ArraysAndStringsChapter1/ZeroMatrix.java
+++ ArraysAndStringsChapter1/ZeroMatrix.java
@@ -17,12 +17,14 @@ public class ZeroMatrix {
         int m = matrix.length;
         int n = matrix[0].length;
         boolean col0 = false, row0 = false;
+
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 col0 = true;
                 break;
             }
         }
+
         for (int j = 0; j < n; j++) {
             if (matrix[0][j] == 0) {
                 row0 = true;
@@ -30,8 +32,8 @@ public class ZeroMatrix {
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
@@ -40,15 +42,8 @@ public class ZeroMatrix {
         }
 
         for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for (int j = 1; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
