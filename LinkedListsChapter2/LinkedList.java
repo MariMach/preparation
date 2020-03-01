907580ea7e0c81455c2815f101924bf92bb9c71e
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-01 21:19:14 +0100

    linked list implemented done

--- LinkedListsChapter2/LinkedList.java
+++ LinkedListsChapter2/LinkedList.java
@@ -1,6 +1,20 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
+
 public class LinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
     private Node first;
     private Node last;
+    private int size;
 
     // addFirst
     // addLast
@@ -9,4 +23,172 @@ public class LinkedList {
     // contains
     // size
     // indexOf
+    // printMylinkedList
+    // isEmpty
+    // toArray
+    // reverse a linked list in place reverse
+    // find the kth node from the last in one pass
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private Node getPrevious(Node node) {
+        Node current = first;
+        while (current != null) {
+            if (current.next == node)
+                return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addLast(int item) {
+        Node node = new Node(item);
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
+
+    public void addFirst(int item) {
+        Node node = new Node(item);
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        Node current = first;
+        while (current != null) {
+            if (current.value == item) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        Node current = first;
+        while (current != null) {
+            if (current.value == item) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean containsUsingIndex(int item) {
+        return indexOf(item) != -1;
+    }
+
+    public void deleteFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (first == last) {
+            first = last = null;
+            size--;
+            return;
+        }
+        Node tmp = first.next;
+        first.next = null;
+        first = tmp;
+        size--;
+    }
+
+    public void deleteLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (first == last) {
+            first = last = null;
+            size--;
+            return;
+        }
+        Node prev = getPrevious(last);
+        prev.next = null;
+        last = prev;
+        size--;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        Node current = first;
+        int index = 0;
+        while (current != null) {
+            array[index] = current.value;
+            index++;
+            current = current.next;
+        }
+        return array;
+    }
+
+    // reverse a linked list in place reverse
+    public void reverse() {
+        if (isEmpty()) {
+            return;
+        }
+        Node prev = first;
+        Node current = first.next;
+        while (current != null) {
+            Node tmp = current.next;
+            current.next = prev;
+            prev = current;
+            current = tmp;
+        }
+        last = first;
+        last.next = null;
+        first = prev;
+    }
+
+    // find the kth node from the last in one pass
+    public int getKthFromTheEnd(int k) {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node start = first;
+        Node end = first;
+        for (int i = 0; i < k - 1; i++) {
+            end = end.next;
+            if (end == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        while (end != last) {
+            end = end.next;
+            start = start.next;
+        }
+        return start.value;
+    }
+
+    public void printMylinkedList() {
+        System.out.println("Your list");
+        if (isEmpty()) {
+            System.out.println("Your list is empty");
+        } else {
+            Node current = first;
+            while (current != null) {
+                System.out.println(current.value);
+                current = current.next;
+            }
+        }
+        System.out.println("End of list");
+    }
+
 }
---
+++ LinkedListsChapter2/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        System.out.println("size :" + list.size());
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(90);
+        list.reverse();
+        int[] array = list.toArray();
+        System.out.println(Arrays.toString(array));
+        list.printMylinkedList();
+        System.out.println(list.indexOf(30));
+        System.out.println(list.indexOf(100));
+        System.out.println(list.contains(400));
+        System.out.println(list.containsUsingIndex(10));
+        list.deleteFirst();
+        list.printMylinkedList();
+        System.out.println("size :" + list.size());
+        System.out.println(list.getKthFromTheEnd(2));
+        System.out.println(list.getKthFromTheEnd(4));
+
+    }
+}
