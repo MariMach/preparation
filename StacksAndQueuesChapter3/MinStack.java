c8fe969bb9e4906d6d0243474949fcf375393bfb
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-04 09:55:07 +0100

    other stacks

---
+++ StacksAndQueuesChapter3/MinStack.java
@@ -0,0 +1,11 @@
+/*
+Design a stack that supports push, pop and retrieving the minimum
+value in constant time.
+For example, we populate our stack with [5, 2, 10, 1] (from left to right).
+stack.min() // 1
+stack.pop()
+stack.min() // 2
+*/
+public class MinStack {
+
+}
---
+++ StacksAndQueuesChapter3/TwoStacks.java
@@ -0,0 +1,20 @@
+
+/*
+1- Implement two stacks in one array.
+Support these operations:
+
+push1() // to push in the first stack
+push2() // to push in the second stack
+pop1()
+pop2()
+isEmpty1()
+isEmpty2()
+isFull1()
+isFull2()
+Make sure your implementation is space efficient.
+(hint: do not allocate the same amount of space by dividing the array in half.)
+*/
+
+public class TwoStacks {
+
+}
