cb8e457cd4a6846e8c0c5751f6340d66eea6e81d
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-08 20:47:06 +0100

    CountPairsWithDiff

--- HashTables/CountPairsWithDiff.java
+++ HashTables/CountPairsWithDiff.java
@@ -1,3 +1,6 @@
+import java.util.Set;
+import java.util.HashSet;
+
 /*
 
 Given an array of integers, count the number of unique pairs of
@@ -11,13 +14,26 @@ that we only want the number of these pairs, not the pairs themselves.
 
 public class CountPairsWithDiff {
 
+    // O(n)
     public static int CountPairsWithDiff(int[] arr, int k) {
         int res = 0;
-
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        for (int i : arr) {
+            if (set.contains(i + k)) {
+                res++;
+            }
+            if (set.contains(i - k)) {
+                res++;
+            }
+            set.remove(i);
+        }
         return res;
     }
 
     public static void main(String args[]) {
-
+        System.out.println(CountPairsWithDiff(new int[] { 1, 7, 5, 9, 2, 12, 3 }, 2));
     }
 }
