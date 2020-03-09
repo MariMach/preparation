56207aa8fbbd1a897ac95f26676a243e84be78f0
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-09 14:42:42 +0100

    rightMostLeaf

--- TreesAndGraphsChapter4/Tree.java
+++ TreesAndGraphsChapter4/Tree.java
@@ -80,11 +80,44 @@ public class Tree {
         if (root == null) {
             return -1;
         }
-        if (root.leftChild == null && root.rightChild == null) {
+        if (isLeaf(root)) {
             return 0;
         }
+
         return 1 + Math.max(height(root.leftChild), height(root.rightChild));
     }
 
     // minimum value in a tree
+    private int min(Node root) {
+        if (isLeaf(root)) {
+            return root.value;
+        }
+        int left = min(root.leftChild);
+        int right = min(root.rightChild);
+
+        return Math.min(root.value, Math.min(left, right));
+    }
+
+    public int min() {
+        return min(root);
+    }
+
+    private boolean isLeaf(Node node) {
+        return root.leftChild == null && root.rightChild == null;
+    }
+
+    // a binary search tree the min is the right most leaf
+    // O(log n)
+    public int rightMostLeaf() {
+        if (root == null) {
+            throw new IllegalStateException();
+        }
+        Node current = root;
+        Node last = current;
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+        return last.value;
+    }
 }
