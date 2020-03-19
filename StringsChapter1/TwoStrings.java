e12163c847a8108386885b0918a8d8ce7dd785ad
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-19 23:12:49 +0100

    TwoStrings

---
+++ StringsChapter1/TwoStrings.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoStrings {
+
+    public static String twoStrings(String s1, String s2) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s1.toCharArray()) {
+            set.add(c);
+        }
+        for (char c : s2.toCharArray()) {
+            if (set.contains(c)) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(twoStrings("hello", "world"));
+        System.out.println(twoStrings("world", "hi"));
+    }
+}
