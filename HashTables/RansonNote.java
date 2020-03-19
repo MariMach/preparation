23e864866e2852f32dc5ded0466546401dc2f32c
MariamMach <39934426+MariamMach@users.noreply.github.com>
2020-03-19 20:31:22 +0100

    RansonNote

--- HashTables/RansonNote.java
+++ HashTables/RansonNote.java
@@ -1,18 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansonNote {
     public static void checkMagazine(String[] magazine, String[] note) {
-        // No
-        // Yes
         Map<String, Integer> map = new HashMap<>();
         for (String s : magazine) {
-            map.put(s, map.getorDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
-        for (Strinf s : note) {
-
+        for (String s : note) {
+            if (map.containsKey(s)) {
+                if (map.get(s) <= 0) {
+                    System.out.println("No");
+                    return;
+                }
+                map.put(s, map.get(s) - 1);
+            } else {
+                System.out.println("No");
+                return;
+            }
         }
         System.out.println("Yes");
     }
 
-    public static void amin() {
+    public static void main(String[] args) {
         String[] s = new String[] { "give", "me", "one", "grand", "today", "night" };
         String[] a = new String[] { "give", "one", "grand", "today" };
 
