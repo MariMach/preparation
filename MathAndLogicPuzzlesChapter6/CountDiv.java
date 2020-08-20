e0247a11d4a260c45a45f6a83ba59614f00ca374
Mariam MACHAALLAH <mariam.machaallah@gmail.com>
2020-08-20 21:54:44 +0100

    count div explained

--- MathAndLogicPuzzlesChapter6/CountDiv.java
+++ MathAndLogicPuzzlesChapter6/CountDiv.java
@@ -23,10 +23,20 @@ public class CountDiv {
          * K) == 0) { res++; } } return res;
          */
         // space and time complexity O(1)
+
+        // It is prefix-sum, B is the upper bound, and A is the lower bound.
+        // First, you count all the divisors available from 1 to upper bound by B/K.
+        // then you count all the divisors available from 1 to lower bound by A/K.
+        // Then you use B/K - A/K, you will get all the divisors from A to B.
+        // But wait, what if A divisible by K, then you will count it 2 times
+        // then you need to check it to make sure it is only counted once.
         return B / K - A / K + (A % K == 0 ? 1 : 0);
     }
 
     public static void main(String[] args) {
+        // 11/2 ===> 2 4 6 8 10
+        // 6/2 ===> 2 4 6
+        // 11/2 - 6/2 + 1 ===> 6 8 10 (6/2 is excluded so we need to reinclud it)
         System.out.println(solution(6, 11, 2)); // 3 cuz 6 8 and 10 are divisible by 2
     }
 }
